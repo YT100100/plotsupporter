e79b35b2d01d5693446666734e85e9e5c70bfec7
Yusuke Toda <yusuke0320.research@gmail.com>
2024-10-15 13:07:27 +0900

    axis_with_rot, heatmap_on_baseplot少し更新

--- R/axis_with_rot.R
+++ R/axis_with_rot.R
@@ -1,6 +1,5 @@
 #' Convert p-value to significance mark
 #'
-#' @export
 #' Add x-axis with vertical labels
 #'
 #' @param at Equivalent to `at` argument in `axis()` function.
@@ -25,3 +24,54 @@ xaxis_with_vert_label <- function(
   par(xpd = FALSE)
 
 }
+
+axis_srt <- function(
+    side, at = NULL, labels = TRUE, srt = NULL, tick = TRUE, line = NA,
+    pos = NA, outer = FALSE, font = NA, lty = 'solid',
+    lwd = 1, lwd.ticks = lwd, col = NULL, col.ticks = NULL,
+    hadj = NA, padj = NA, gap.axis = NA, ...) {
+
+  # check arguments
+  stopifnot(length(side) == 1)
+
+  # if srt is not given or labels == FALSEs,
+  # this function works as a standard `axis()`
+  if (is.null(srt) | identical(labels, FALSE)) {
+    return(axis(side, at, labels, tick, line, pos, outer, font, lty, lwd,
+                lwd.ticks, col, col.ticks, hadj, padj, gap.axis, ...))
+  }
+
+  # draw axis without labels
+  at <- axis(
+    side, at, labels = FALSE, tick, line, pos, outer, font, lty,
+    lwd, lwd.ticks, col, col.ticks, hadj, padj, gap.axis, ...)
+
+  # determine coordinate of labels
+  # yをどう決めるか？
+  labloc <- aaaa
+
+  # add text
+  if (identical(side, 1)) {
+
+    if (srt < 45) {
+      if (identical(hadj, NA)) hadj <- 0
+      if (identical(padj, NA)) padj <- 0
+    }
+    if (identical(labels, TRUE)) labels <- as.character(at)
+    text(x = at, y = labloc, labels = labels)
+
+  }
+  stopifnot(length(srt) == 1)
+
+  # if (is.null(srt)) {
+  #   srt <-
+  # }
+
+
+}
+
+plot(0, xaxt = 'n', yaxt = 'n')
+axis_srt(1)
+axis_srt(2)
+axis_srt(3)
+axis_srt(4)
